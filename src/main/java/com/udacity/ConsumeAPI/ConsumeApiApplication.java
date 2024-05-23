dc9bf3d89a34435d105cb8b8078fa3f45999ed3f
HussamAltalib <hussamaltalib@outlook.com>
2024-05-23 15:47:59 +0300

    Lab: Consume an API 'Consume Breaking Bad quotes API'

--- src/main/java/com/udacity/ConsumeAPI/ConsumeApiApplication.java
+++ src/main/java/com/udacity/ConsumeAPI/ConsumeApiApplication.java
@@ -1,13 +1,58 @@
 package com.udacity.ConsumeAPI;
 
+import com.udacity.ConsumeAPI.Entity.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
 
 @SpringBootApplication
 public class ConsumeApiApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumeApiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumeApiApplication.class, args);
 	}
 
+	//Adding more code to disabled SSL verification
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		try {
+			TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+			SSLContext sslContext = SSLContexts.custom()
+					.loadTrustMaterial(null, acceptingTrustStrategy)
+					.build();
+			CloseableHttpClient httpClient = HttpClients.custom()
+					.setSSLContext(sslContext)
+					.build();
+			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+			return new RestTemplate(factory);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote[] quotes = restTemplate.getForObject(
+					"https://api.breakingbadquotes.xyz/v1/quotes", Quote[].class);
+			for (Quote quote : quotes) {
+				log.info(quote.toString());
+			}
+		};
+	}
 }
---
+++ src/main/java/com/udacity/ConsumeAPI/Entity/Quote.java
@@ -0,0 +1,50 @@
+package com.udacity.ConsumeAPI.Entity;
+
+public class Quote {
+    private long id;
+    private String quote;
+    private String author;
+
+    public Quote() {
+
+    }
+
+    public Quote(long id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
